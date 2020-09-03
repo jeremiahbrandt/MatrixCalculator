fd2f5003733432c9c23b37f8f500488cca4338e1
Jeremiah Brandt <jeremiahjbrandt@outlook.com>
2020-09-03 13:41:28 -0500

    Created CalculationButton.java

---
+++ src/main/java/com/brandt/jeremiah/CalculationButton.java
@@ -0,0 +1,16 @@
+package com.brandt.jeremiah;
+
+import javafx.scene.control.Button;
+
+public class CalculationButton extends Button {
+    private static final int WIDTH = 75;
+    private static final double HEIGHT = 75;
+
+    public CalculationButton(Calculator calculator) {
+        super.setText("=");
+        super.setMinSize(WIDTH, HEIGHT);
+        super.setOnMouseClicked(mouseEvent -> {
+            calculator.calculate(Operation.ADDITION);
+        });
+    }
+}
